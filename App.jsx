f4eefc577fd191aedfb4bff1451e1414cbffbbb4
Oskar Szmurło <149005278+szmurk0@users.noreply.github.com>
2024-06-09 18:58:34 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  // Ustawianie stanów do przechowywania wartości wejściowej i wyniku
+  const [wartoscWejsciowa, ustawWartoscWejsciowa] = useState('');
+  const [wynik, ustawWynik] = useState('');
+
+  // Funkcja konwertująca numerał rzymski na liczbę arabską
+  function rzymskieNaArabskie(rzymski) {
+    const mapaNumeralRzymskie = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+    
+    let suma = 0;
+    let poprzedniaWartosc = 0;
+    
+    // Iteracja przez numerał rzymski od końca do początku
+    for (let i = rzymski.length - 1; i >= 0; i--) {
+      const aktualnaWartosc = mapaNumeralRzymskie[rzymski[i].toUpperCase()];
+      
+      // Jeśli aktualna wartość jest mniejsza niż poprzednia, odejmujemy ją od sumy
+      if (aktualnaWartosc < poprzedniaWartosc) {
+        suma -= aktualnaWartosc;
+      } else {
+        // W przeciwnym razie dodajemy ją do sumy
+        suma += aktualnaWartosc;
+      }
+      
+      poprzedniaWartosc = aktualnaWartosc;
+    }
+    
+    return suma;
+  }
+
+  // Funkcja konwertująca liczbę arabską na numerał rzymski
+  function arabskieNaRzymskie(arabski) {
+    const mapaNumeralRzymskie = [
+      ['M', 1000],
+      ['CM', 900],
+      ['D', 500],
+      ['CD', 400],
+      ['C', 100],
+      ['XC', 90],
+      ['L', 50],
+      ['XL', 40],
+      ['X', 10],
+      ['IX', 9],
+      ['V', 5],
+      ['IV', 4],
+      ['I', 1]
+    ];
+    
+    let wynik = '';
+    // Iteracja przez mapę numerałów rzymskich i budowanie wyniku
+    for (const [rzymski, wartosc] of mapaNumeralRzymskie) {
+      while (arabski >= wartosc) {
+        wynik += rzymski;
+        arabski -= wartosc;
+      }
+    }
+    return wynik;
+  }
+
+  // Funkcja obsługująca konwersję
+  function Konwertuj() {
+    if (/^\d+$/.test(wartoscWejsciowa)) {
+      // Jeśli wartość wejściowa jest liczbą arabską, konwertuj na rzymską
+      const skonwertowanaWartosc = arabskieNaRzymskie(parseInt(wartoscWejsciowa, 10));
+      ustawWynik(skonwertowanaWartosc);
+    } else {
+      // W przeciwnym razie konwertuj numerał rzymski na arabską
+      const skonwertowanaWartosc = rzymskieNaArabskie(wartoscWejsciowa);
+      ustawWynik(skonwertowanaWartosc);
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Witam w konwerterze numerałów!
+      </h1>
+
+      <p>Podaj numerał rzymski lub liczbę arabską: </p>
+      <input 
+        type="text" 
+        id="numeral" 
+        value={wartoscWejsciowa} 
+        onChange={(e) => ustawWartoscWejsciowa(e.target.value)} 
+      />
+      
+      <input 
+        type="submit" 
+        value="Konwertuj" 
+        onClick={Konwertuj} 
+      />
+
+      <p id="wynik">{wynik}</p>
+    </div>
+  );
+}
+
+export default App;
